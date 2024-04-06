2bdc0ca9a69c8337cebbe1e9825c6abf6a55579f
Michael <mchlmiller89@gmail.com>
2024-04-06 09:08:23 -0700

    Added custom properties, linked to Rest Controller, and updated applications.property with custon port/pathing.

--- src/main/java/com/luv2code/mycoolapp/rest/FunRestController.java
+++ src/main/java/com/luv2code/mycoolapp/rest/FunRestController.java
@@ -1,10 +1,20 @@
 package com.luv2code.mycoolapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+    @Value("${username.name}")
+    private String userName;
+    @Value("${team.name}")
+    private String teamName;
+
+    @GetMapping("/teaminfo")
+    public String getTeamInfo(){
+        return "Username: " + userName + ", Team Name: " + teamName;
+    }
 
     @GetMapping("/")
     public String sayHello(){
